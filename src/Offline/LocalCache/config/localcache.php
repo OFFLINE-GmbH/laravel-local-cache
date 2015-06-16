b0d85bdf62aab21a39473d62b5c5ad64c9a777ef
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2015-06-16 15:04:43 +0200

    added "in seconds" to clarify

--- src/Offline/LocalCache/config/localcache.php
+++ src/Offline/LocalCache/config/localcache.php
@@ -3,9 +3,9 @@ return [
     // Base URL used for replacements.
     'base_url'     => base_url(),
     // Route path to serve cached files
-    'route'        => 'cache', // base_url/cache/{hash}
+    'route'        => 'cache', // ${base_url}/cache/${hash}
     // Where to store the cached files
     'storage_path' => storage_path('localcache'),
-    // Time to live for cached files
-    'ttl'          => 20,
+    // Time to live for cached files in seconds
+    'ttl'          => 3600,
 ];
